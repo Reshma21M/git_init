6f2745228001a8a29f07f701a660cc8712623651
Reshma <reshmamohamed2023@gmail.com>
2025-03-07 13:38:15 +0530

    first commit

---
+++ cppfile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    // Ask the user to input a number
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Check if the number is even or odd
+    if (num % 2 == 0) {
+        cout << num << " is even." << endl;
+    } else {
+        cout << num << " is odd." << endl;
+    }
+
+    return 0;
+}
+
