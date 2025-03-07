8ffeb78050aa58ff5e534dddcc7c46a9034dc8c3
Reshma <reshmamohamed2023@gmail.com>
2025-03-07 13:52:31 +0530

    changed

--- cppfile.cpp
+++ cppfile.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> num;
 
     // Check if the number is even or odd
-    if (num % 2 == 0) {
+    if (num % 10 == 0) {
         cout << num << " is even." << endl;
     } else {
         cout << num << " is odd." << endl;
